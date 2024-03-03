4100c9fb6e22fcc970ebbade7f44a16c2d9bc19a
Yusuf Pertex <yusuf.expert.official@gmail.com>
2024-03-03 11:37:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "100vw";
+    document.body.style.marginLeft = "100vw";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.body.style.marginLeft = "0";
+}
+
+let i = 0
+let txt = 'Welcome to Yusuf Official Portfolio Website'
+
+function typing() {
+    if (i < txt.length) {
+        document.getElementById("typing").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+    }
+}
+
+function scrollbar() {
+    let winscroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = ( winscroll / height ) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+window.onload = typing
+window.onscroll = scrollbar
